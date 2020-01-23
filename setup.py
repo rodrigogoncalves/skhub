1c197d40f4560a32c7f6e517f4913aabc2a8100f
Rodrigo Goncalves <rodrigogoncalves@users.noreply.github.com>
2020-01-23 12:08:24 +0000

    Add setup.py mainly for documenting

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="skhub",
+    version="1.0.0",
+    author="Rodrigo Goncalves",
+    author_email="rodrigogoncalves@users.noreply.github.com",
+    description="A simple exercise with prime numbers",
+    url="https://github.com/rodrigogoncalves/skhub.git",
+    packages=find_packages(),
+    python_requires='>=3.6',
+)

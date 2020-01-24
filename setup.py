6be45aa80ca6ada771459835e855d02724afb5a5
Rodrigo Goncalves <rodrigogoncalves@users.noreply.github.com>
2020-01-24 14:51:28 +0000

    Add tag 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="skhub",
-    version="1.0.0",
+    version="0.1.0",
     author="Rodrigo Goncalves",
     author_email="rodrigogoncalves@users.noreply.github.com",
     description="A simple exercise with prime numbers",
